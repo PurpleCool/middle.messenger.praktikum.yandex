6a33aaa0b3ce51c79fed106b8319cf724c22a84e
PurpleCool <mariagruzintseva7@gmail.com>
2022-02-22 01:41:31 +0300

    add handlebars template for error 500 page

--- src/pages/errors/error500/error500.js
+++ src/pages/errors/error500/error500.js
@@ -1,13 +1,15 @@
 import Handlebars from 'handlebars'
+import template from './index.tmpl'
 
-(function() {
+document.addEventListener('DOMContentLoaded', () => {
+    let compiled = Handlebars.compile(template);
+
     let data = {
         status: '500',
         title: "Кто-то что-то нажал, а мы чиним",
         goBackLink: "Назад к чатам"
     };
 
-    let template = Handlebars.compile(document.getElementById('template').innerHTML);
-    let filled = template(data);
-    document.getElementById('app').innerHTML = filled;
-})()
+    let html = compiled(data);
+    document.getElementById('app').innerHTML = html;
+});
---
+++ src/pages/errors/error500/index.tmpl.js
@@ -0,0 +1,7 @@
+// language=hbs
+
+export default ` 
+    <h1>{{status}}</h1>
+    <h2>{{title}}</h2>
+    <a href="#">{{goBackLink}}</a>
+`;
