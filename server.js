73511e04c4a0426dcb6f17705c0a3b2f507e6622
PurpleCool <mariagruzintseva7@gmail.com>
2022-03-01 01:06:19 +0300

    fixed server

--- server.js
+++ server.js
@@ -1,18 +1,12 @@
 const express = require("express");
-const Bundler = require("parcel-bundler");
 
 const PORT = 3000;
-
 const app = express();
+app.use(express.static("dist"));
 
-const bundler = new Bundler("static/index.html", { watch: true, cache: false });
-
-app.use(bundler.middleware());
-
-
-app.get("*",function(req,res,next){
-  res.sendFile(__dirname + 'static/index.html');
-})
+app.get("*", (req, res) =>
+  res.sendFile("index.html", { root: __dirname + "/dist" })
+);
 
 app.listen(PORT, function () {
   console.log(`listening on: http://localhost:${PORT}`);
